328bda391a0ad2c14ab08608c83ccf2f3f76b87b
rav_official <97645551+ravoffc@users.noreply.github.com>
2022-02-23 15:44:26 +0700

    Add files via upload

---
+++ components/contact.vue
@@ -0,0 +1,80 @@
+<template>
+<main>
+    <div>
+    <div class="jumbotron">
+    <h2 class="text-center">Contact Us</h2>
+	</div>
+    <div class="row justify-content-center">
+		<div class="col-12 col-md-8 col-lg-6 pb-5">
+
+
+                    <!--Form with header-->
+
+                    <form action="mail.php" method="post">
+                        <div class="card border-primary rounded-0">
+                            <div class="card-header p-0">
+                                <div class="bg-info text-white text-center py-2">
+                                    <h3>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                                        </svg>
+                                    </h3>
+                                    <p class="m-0">Isilah Dengan Teliti Dan Pastikan Benar !!!</p>
+                                </div>
+                            </div>
+                            <div class="card-body p-3">
+                                <form action="mailto:someone@example.com" method="post" autocomplete="off">
+                                    <div class="form-group">
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                                                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                                        <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+                                                        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <input type="text" class="form-control" id="name" name="username" placeholder="Username" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <input type="email" class="form-control" id="email" name="email" placeholder="your_example@gmail.com" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-dots-fill" viewBox="0 0 16 16">
+                                                        <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <textarea class="form-control" placeholder="Your message" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <input type="submit" value="Send" class="btn btn-info btn-block rounded-0 py-2">
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </form>
+                    <!--Form with header-->
+
+
+                </div>
+	</div>
+</div>
+</main>
+</template>
---
+++ pages/heading.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row align-items-center">
+            <!-- Column 1-->
+                <div class="col-md-6">
+                    <h1 class="mb-4 font-weight-bold">Welcome To
+                        <span class="text-info">Website</span><br>Online Store and Entertaiment.
+                    </h1>
+                    <p class="mb-4 pb-2 ">
+                        hopefully what you want is available on our website. We also look forward to your input and suggestions so that our website will be even better.
+                    </p>
+                    <div>
+                        <a href="contact.html" class="text-center btn btn-outline-dark btn-lg btn-block">Contact Us</a>
+                    </div>
+                </div>
+            <!-- Column 1 -->
+            <!-- Column 2 -->
+            <div class="col-md-6 d-none d-sm-none d-md-block">
+                <img src="~/assets/foto.jpg" alt="tours pics" class="img-fluid">
+            </div>
+            <!-- Column 2 -->
+            </div>
+        </div>
+    </main>
+</template>
